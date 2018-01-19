b0c8803397c056611dabe2a7e10b1d38027ae41e
mohdyuna4 <35587736+mohdyuna4@users.noreply.github.com>
2018-01-19 10:37:08 +0530

    Add files via upload

---
+++ 2pro.py
@@ -0,0 +1,6 @@
+#solutions
+a=float(input('enter a value'))
+b=float(input('enter a value'))
+c=a+b
+print(c)
+
---
+++ program1.py
@@ -0,0 +1,8 @@
+#my first program
+print ("arif")
+#description
+print ('i m here to enjoy python')
+#hobbies
+print('cricket')
+print('200+20')
+
