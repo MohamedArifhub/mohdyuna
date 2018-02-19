baf82336df175113e8b978968256bed27fa18d03
mohdyuna4 <35587736+mohdyuna4@users.noreply.github.com>
2018-02-19 14:42:55 +0530

    Add files via upload

---
+++ session_5data types.py
@@ -0,0 +1,121 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: /home/ec2017b203/arif/5 datatypes.py ===============
+5 is of type <class 'int'>
+2.0 is of type <class 'float'>
+(1+2j) is complex number? True
+>>> 
+=============== RESTART: /home/ec2017b203/arif/5 datatypes.py ===============
+5 is of type <class 'int'>
+2.0 is of type <class 'float'>
+(1+2j) is complex number? False
+>>> 
+=============== RESTART: /home/ec2017b203/arif/5 datatypes.py ===============
+5 is of type <class 'int'>
+2.0 is of type <class 'float'>
+(1+2j) is complex number? False
+>>> 
+=============== RESTART: /home/ec2017b203/arif/5 datatypes.py ===============
+5 is of type <class 'int'>
+2.0 is of type <class 'float'>
+(1+2j) is complex number? True
+>>> 
+=============== RESTART: /home/ec2017b203/arif/5 datatypes.py ===============
+5 is of type <class 'int'>
+2.0 is of type <class 'float'>
+(1+2j) is complex number? False
+>>> int(1+2)
+3
+>>> int("999+"889")
+    
+SyntaxError: invalid syntax
+>>> 
+=============== RESTART: /home/ec2017b203/arif/5 datatypes.py ===============
+5 is of type <class 'int'>
+2.0 is of type <class 'float'>
+(1+2j) is complex number? False
+>>> 
+================ RESTART: /home/ec2017b203/arif/5_DATA_NUM.py ================
+5 is of type <class 'int'>
+2.0 is of type <class 'float'>
+(1+2j) is complex number? False
+>>> 
+================ RESTART: /home/ec2017b203/arif/5_DATA_NUM.py ================
+5 is of type <class 'int'>
+2.0 is of type <class 'float'>
+(1+2j) is complex number? False
+>>> 
+================ RESTART: /home/ec2017b203/arif/5_DATA_NUM.py ================
+5 is of type <class 'int'>
+2.0 is of type <class 'float'>
+(1+2j) is complex number? False
+4 is of type <class 'int'>
+78.2 is of type <class 'float'>
+Traceback (most recent call last):
+  File "/home/ec2017b203/arif/5_DATA_NUM.py", line 17, in <module>
+    print(b,"is a float number?", isinstance(7-j,float))
+NameError: name 'j' is not defined
+>>> 
+================ RESTART: /home/ec2017b203/arif/5_DATA_NUM.py ================
+5 is of type <class 'int'>
+2.0 is of type <class 'float'>
+(1+2j) is complex number? False
+4 is of type <class 'int'>
+78.2 is of type <class 'float'>
+(5-2j) is a float number? False
+>>> 
+================ RESTART: /home/ec2017b203/arif/5_DATA_NUM.py ================
+5 is of type <class 'int'>
+2.0 is of type <class 'float'>
+(1+2j) is complex number? False
+4 is of type <class 'int'>
+78.2 is of type <class 'float'>
+(5-2j) is a float number? False
+>>> 
+================ RESTART: /home/ec2017b203/arif/5_DATA_NUM.py ================
+5 is of type <class 'int'>
+2.0 is of type <class 'float'>
+(1+2j) is complex number? True
+4 is of type <class 'int'>
+78.2 is of type <class 'float'>
+(5-2j) is a float number? True
+>>> L=[90,45,89,"36",78]
+>>> L=[3]
+>>> L=[90,45,89,"36",78]
+>>> L[3]
+'36'
+>>> L[0]
+90
+>>> L[4]
+78
+>>> tup1=[89,90,87,"hello"]
+>>> tup1[0]
+89
+>>> tup2[3]
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    tup2[3]
+NameError: name 'tup2' is not defined
+>>> tup1[3]
+'hello'
+>>> tup[4]
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    tup[4]
+NameError: name 'tup' is not defined
+>>> s=['hi im arif']
+>>> s[5]
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    s[5]
+IndexError: list index out of range
+>>> s[0]
+'hi im arif'
+>>> s='hi','im','arif']
+SyntaxError: invalid syntax
+>>> s='hi','im','arif'
+>>> s[2]
+'arif'
+>>> 
