4371e6323372beb6dbc3029f34a11f99efbd7d95
mohdyuna4 <35587736+mohdyuna4@users.noreply.github.com>
2018-02-23 10:14:06 +0530

    Add files via upload

---
+++ see7ion.py
@@ -0,0 +1,31 @@
+print('using add funtion')
+def add(p,q):
+    return p+q
+print((add(8,5)))
+
+print('using subtract funtion')
+def sub(m,n):
+    return m-n
+print((sub(67,54)))
+
+print('using multipication funtion')
+def mul(k,l):
+    return k*l
+print((mul(2,5)))
+
+print('using division funtion')
+def div(g,h):
+    return g//h
+d=(div(600,5))
+print(d)
+
+print('using modulo funtion')
+def mod(a,s):
+    return a%s
+v=(mod(5,4))
+print(v)
+
+print('using exponent funtion')
+def exponent(o,q):
+    return o**q
+print(exponent(35,9))
