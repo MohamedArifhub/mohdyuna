6436adc89cdd7c89f4e27c24c060746771e954b3
mohdyuna4 <35587736+mohdyuna4@users.noreply.github.com>
2018-03-09 10:32:57 +0530

    Add files via upload

---
+++ file_m.py
@@ -0,0 +1,14 @@
+p=open("2.txt","w") # creating the text file.
+p.write("Hello friends")# texting into a file.
+p.write
+
+p=open("2.txt","r")# access to readabilty.
+p.read()
+
+p=open("2.txt","a")# appending the texted script.
+p.write(" whatsup")
+
+p=open("2.txt","r")# reading th appened text.
+p.read()
+
+p.close()# close the text file
